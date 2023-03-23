3ff0482b0eef26f3fab0498214c917271139c73d
Ivan Hansen <66189578+IHansen225@users.noreply.github.com>
2023-03-23 15:54:21 -0300

    Update 0-lockboxes.py

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -6,14 +6,15 @@
     Write a method that determines if all the boxes can be opened.
 """
 
+
 def canUnlockAll(boxes):
     """
         Check if all boxes can be unlocked
     """
     n = len(boxes)
-    unlocked = [False] * n  # keep track of whether a box is unlocked or not
-    unlocked[0] = True  # the first box is already unlocked
-    stack = [0]  # start with the first box
+    unlocked = [False] * n
+    unlocked[0] = True
+    stack = [0]
 
     while stack:
         box = stack.pop()
