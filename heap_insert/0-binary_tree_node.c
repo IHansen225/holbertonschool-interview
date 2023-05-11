1043538ed9727f2b3e87e09fafc2f162b898a819
Ihansen225 <ihk225@gmail.com>
2023-05-11 19:11:36 -0300

    Interview

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -9,13 +9,12 @@
  * Return: pointer to the new node, or NULL on failure
  */
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) 
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-	if (new_node == NULL) {
+	if (new_node == NULL) 
 		return (NULL);
-	}
 
 	new_node->n = value;
 	new_node->parent = parent;
