60aa1e7e31804cbb255678a0e30cfc220e4ecd91
Ihansen225 <ihk225@gmail.com>
2023-05-11 20:03:22 -0300

    Corrections

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+    Minimum Operations
+"""
+
+
+def minOperations(n):
+    """
+        Method that calculates the fewest number of operations
+        needed to result in exactly n H characters in the file.
+    """
+    op = 0
+    chrs = 1
+    if n <= 1:
+        return op
+    while chrs < n:
+        if n % chrs == 0:
+            op += 2
+            chrs += chrs
+        else:
+            op += 1
+            chrs += chrs
+    return op
