04148f36fb800762d3c55d1ec7bea2d4a99df772
Ihansen225 <ihk225@gmail.com>
2023-05-11 21:09:45 -0300

    Corrections

--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -5,7 +5,6 @@
 #include <stdlib.h>
 
 int is_stable(int grid[3][3]);
-void print_grid(int grid[3][3]);
 void topple(int grid[3][3], int, int);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 
